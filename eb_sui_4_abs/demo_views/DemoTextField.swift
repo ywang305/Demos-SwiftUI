96239ca25427b72927935f288a263ee9c3349489
yao <ywang305@syr.edu>
2020-02-27 10:34:32 -0500

    nothing essential

--- eb_sui_4_abs/demo_views/DemoTextField.swift
+++ eb_sui_4_abs/demo_views/DemoTextField.swift
@@ -9,12 +9,16 @@
 import SwiftUI
 
 struct DemoTextField: View {
+    @ObservedObject private var kbState = KBStateUtil()
     @State private var name = "Unknown"
     var body: some View {
         VStack {
             Text("Hello \(name)")
             TextField("placeholder", text: $name).textFieldStyle(RoundedBorderTextFieldStyle())
         }
+    }
+    var debug: Any {
+        print(self.kbState.kbRect)
     }
 }
 
@@ -23,7 +27,10 @@ struct DemoSecureField: View {
     var body: some View {
         VStack {
             Text("Your secret \(password)")
-            SecureField("placeholder", text: $password){}.textFieldStyle(RoundedBorderTextFieldStyle())
+            SecureField("placeholder", text: $password){}
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .overlay(RoundedRectangle(cornerRadius: 4)
+                    .stroke(Color.blue, lineWidth: 1))
         }
     }
 }
@@ -34,5 +41,49 @@ struct DemoTextField_Previews: PreviewProvider {
             DemoTextField()
             DemoSecureField()
         }
+    }
+}
+
+
+final class KBStateUtil: ObservableObject {
+    private var notificationCenter: NotificationCenter
+    @Published private(set) var currentHeight: CGFloat = 0
+    
+    @Published var kbRect: CGRect = .zero
+
+    init(center: NotificationCenter = .default) {
+        notificationCenter = center
+        notificationCenter.addObserver(self, selector: #selector(keyBoardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(keyBoardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    deinit {
+        notificationCenter.removeObserver(self)
+    }
+    
+    func keyboardState(for userInfo:[AnyHashable: Any], in v: UIView?)-> CGRect {
+        var rold = userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! CGRect
+        var rnew = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+
+        if let v = v {
+            let co = UIScreen.main.coordinateSpace
+            rold = co.convert(rold, to: v)
+            rnew = co.convert(rnew, to: v)
+
+            return rnew.intersection(v.bounds)
+        }
+        return .zero
+    }
+
+    @objc private func keyBoardWillShow(notification: Notification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect) {
+            currentHeight = keyboardSize.height
+            kbRect = keyboardSize
+        }
+    }
+
+    @objc private func keyBoardWillHide(notification: Notification) {
+        currentHeight = 0
+        kbRect = .zero
     }
 }
