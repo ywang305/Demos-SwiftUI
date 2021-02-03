496fda22abdd5ff4f43dc8682d5b14e610a267e7
yao <ywang305@syr.edu>
2021-02-02 23:13:41 -0500

    @state re-render test

---
+++ eb_sui_4_abs/demo_features/StateTest.swift
@@ -0,0 +1,45 @@
+//
+//  StateTest.swift
+//  eb_sui_4_abs
+//
+//  Created by Yao Wang on 2/2/21.
+//  Copyright © 2021 yao. All rights reserved.
+//
+
+import SwiftUI
+
+struct User {
+    var username = ""
+    var email = ""
+}
+
+
+struct StateTest: View {
+    @State private var user: User = User(username: "Yao", email: "yao@apple.com")
+    
+    var body: some View {
+        VStack {
+            Text(user.username)
+            Text(user.email)
+            Button("Change field inside user object to re-render") {
+                user.username += " Wang";
+            }.padding().overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.purple, lineWidth: 5)
+            )
+            Button("Change user object to re-render") {
+                user = User(username: user.username+"Chen", email: "chen@google.com")
+            }.padding().overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.purple, lineWidth: 5)
+            )
+        }
+        
+    }
+}
+
+struct StateTest_Previews: PreviewProvider {
+    static var previews: some View {
+        StateTest()
+    }
+}
